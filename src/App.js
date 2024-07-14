5f88f036dfe108dae7742f2c0305c6e8aa9a55c9
Ankit Yadav <ankityadav60062@gmail.com>
2024-07-14 12:35:06 +0530

    toast added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { apiUrl , filterData } from './data';
 import Navbar from './Component/Navbar'
 import Filter from './Component/Filter';
-
+// import { useState , useEffect } from 'react';
+///import { useEffect } from 'react';
+import Spinner from './Component/Spinner';
+import Card from './Component/Card';
+import Cards from './Component/Cards';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
+  
+  const [courses , setCourses] = useState(null);
+  const [loading , setLoading]= useState(true);
+  async function fetchData()
+  
+  {
+    setLoading(true);
+    try
+    {
+   let response = await fetch(apiUrl);
+   let output = await response.json();
+   setCourses(output);
+    }
+    catch(error)
+    {
+       toast.error("Api not working");
+    }
+    setLoading(false);
+  }
+  useEffect(()=> {
+
+    fetchData(); }, []
+  )
   return (
     <div>
     <div>
@@ -12,6 +41,11 @@ const App = () => {
     <div>
       <Filter filterData={filterData}> </Filter>
     </div>
+    <div>
+      {
+      loading ? (<spinner></spinner>) : (<Cards></Cards>)
+      }
+      </div>
     </div>
   )
 }
